3287b2199ad593ec754351413b083269ac9bf6d6
Dreen <dreen1@gmail.com>
2013-06-03 17:50:44 +0000

    getting the message value

--- echo/echo.js
+++ echo/echo.js
@@ -1,19 +1,19 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to echo.";
-  };
+Messages = new Meteor.Collection("messages");
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
+if (Meteor.isClient)
+{
+        Template.MsgBox.events(
+        {
+                'click button' : function ()
+                {
+                        var msg = document.getElementTagName("input")[0].value;
+                }
+        });
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+if (Meteor.isServer)
+{
+        Meteor.startup(function () {
+                // code to run on server at startup
+        });
 }
